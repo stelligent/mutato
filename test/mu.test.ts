d4320ec2af1579569a393fa276f4f3354431d3c4
Sepehr Laal <sepehr.laal@gmail.com>
2020-01-16 21:25:20 -0800

    replaced jest with mocha + added coverage
    this addresses #4 and #5

--- test/mu.test.ts
+++ test/mu.test.ts
@@ -2,7 +2,8 @@ import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core';
 import Mu = require('../lib/mu-stack');
 
-test('Empty Stack', () => {
+describe('Empty Stack', () => {
+  it('should have no resources', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new Mu.MuStack(app, 'MyTestStack');
@@ -10,4 +11,5 @@ test('Empty Stack', () => {
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
     }, MatchStyle.EXACT))
+  })
 });
---
+++ test/sanity.test.ts
@@ -0,0 +1,15 @@
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+chai.use(chaiAsPromised);
+
+describe('Sanity Check', () => {
+  it('should always pass sync', () => {
+    chai.assert(2+2 === 4);
+  });
+
+  it('should always pass async', async () => {
+    await chai.assert.isFulfilled(Promise.resolve());
+    await chai.assert.isRejected(Promise.reject());
+  });
+})
