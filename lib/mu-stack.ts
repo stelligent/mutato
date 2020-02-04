befa7a1c481ab51f119bc52a081703dcb4ef7fe7
Trevor <trevormillner@gmail.com>
2020-02-03 23:41:15 -0800

    task - Address CR comments (documentation++)

--- lib/mu-stack.ts
+++ lib/mu-stack.ts
@@ -1,6 +1,16 @@
 import * as cdk from '@aws-cdk/core';
 
+/**
+ * @class MuStack - primary stack
+ */
 export class MuStack extends cdk.Stack {
+  /**
+   * Create a new stack
+   *
+   * @param scope
+   * @param id
+   * @param props
+   */
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
   }
