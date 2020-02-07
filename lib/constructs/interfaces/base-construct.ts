1d964532aed4e041347f6496eedc42b0e88f52db
Sepehr Laal <sepehr.laal@gmail.com>
2020-02-07 13:13:15 -0800

    broke this down to a class and its interface

--- lib/constructs/interfaces/base-construct.ts
+++ lib/constructs/interfaces/base-construct.ts
@@ -2,10 +2,19 @@ import * as cdk from '@aws-cdk/core';
 import * as packageJson from '../../../package.json';
 
 /**
+ * This interface is not exported on purpose! users must use the abstract class
+ * instead. The abstract class has a minimal default implementation.
+ */
+interface IBaseConstruct extends cdk.Construct {
+  initialize(): Promise<void>;
+}
+
+/**
  * Base class of all Mu managed constructs
  * It allows for async initialization of its CDK components
  */
-export abstract class IBaseConstruct extends cdk.Construct {
+export abstract class BaseConstruct extends cdk.Construct
+  implements IBaseConstruct {
   /**
    * @hideconstructor
    * @param {cdk.Construct} scope CDK scope
@@ -18,6 +27,12 @@ export abstract class IBaseConstruct extends cdk.Construct {
     cdk.Tag.add(this, 'mu:version', packageJson.version);
   }
 
-  /** subclasses can use this to perform async initialization */
-  public abstract async initialize(): Promise<void>;
+  /**
+   * subclasses can use this to perform async initialization
+   *
+   * @returns {void} nothingness. throws if unsuccessful
+   */
+  public initialize(): Promise<void> {
+    return Promise.resolve();
+  }
 }
