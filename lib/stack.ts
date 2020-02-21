8279af4f07e402ad35fc96977f3299f3ee251b32
Sepehr Laal <sepehr.laal@gmail.com>
2020-02-20 21:26:30 -0800

    first pass implementation at docker image building

--- lib/stack.ts
+++ lib/stack.ts
@@ -9,14 +9,13 @@ import * as _ from 'lodash';
 import * as parseGithubUrl from 'parse-github-url';
 import * as path from 'path';
 import { config, flatten as flattenedConfig } from './config';
+import { container, network } from './constructs';
 import { Parser } from './parser';
-import { Registry } from './registry';
 
 /**
  * The Mu app stack, everything inside a mu.yml
  */
 export class MuApp extends cdk.Stack {
-  private readonly registry = new Registry(this);
   private readonly parser = new Parser();
   private readonly log: debug.Debugger;
 
@@ -63,8 +62,112 @@ export class MuApp extends cdk.Stack {
    */
   public async fromObject(muYml: object): Promise<void> {
     this.log('creating stack from object: %o', muYml);
-    const networkProps = _.get(muYml, 'mu.network', {});
-    await this.registry.create('network', 'network', networkProps);
+    // helper lambda to query mu.yml for a specific construct type
+    const queryByType = (type: string): object[] =>
+      _.get(muYml, 'mu', [])
+        .filter((c: object[]) => _.head(_.keys(c)) === type)
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore
+        .map(c => c[type]);
+
+    // create the base network construct
+    const networks = queryByType('network');
+    assert.ok(networks.length <= 1);
+    const networkProps = _.head(networks);
+    const networkConstruct = new network(this, 'network', networkProps);
+    await networkConstruct.initialize();
+
+    // create the pipeline that builds and pushes all our containers
+    const containers = queryByType('container');
+    if (containers.length > 0) {
+      this.log('synthesizing Mu pipeline');
+      const pipeline = new codePipeline.Pipeline(this, 'MuPipeline', {
+        restartExecutionOnUpdate: true
+      });
+
+      this.log('attempting to extract local Github metadata');
+      const params = parseGithubUrl(config.opts.git.remote);
+      const branch = config.opts.git.branch;
+      this.log('deploying the branch "%s" in repository: %o', branch, params);
+
+      /** @todo properly handle non Github repositories */
+      assert.ok(params != null && params.owner && params.repo);
+
+      const sourceOutput = new codePipeline.Artifact();
+      const source = new codePipelineActions.GitHubSourceAction({
+        actionName: 'GitHub',
+        output: sourceOutput,
+        owner: params?.owner as string,
+        repo: params?.name as string,
+        branch,
+        oauthToken: cdk.SecretValue.plainText(
+          /** @todo add SSM here to read github token from */
+          config.opts.git.secret
+        )
+      });
+      pipeline.addStage({
+        stageName: 'Mu-Source',
+        actions: [source]
+      });
+
+      const wrapVal = (value: string): codeBuild.BuildEnvironmentVariable => {
+        return {
+          type: codeBuild.BuildEnvironmentVariableType.PLAINTEXT,
+          value
+        };
+      };
+
+      this.log('forwarding configuration to CodeBuild: %o', flattenedConfig);
+      const wrappedFlattenedConfig = _.mapValues(flattenedConfig, wrapVal);
+
+      const buildActions: codePipeline.IAction[] = [];
+      (containers as container[]).forEach(container => {
+        const project = new codeBuild.PipelineProject(
+          this,
+          // TODO fixme
+          `CodeBuild-${_.get(container, 'name', 'default')}`,
+          {
+            environment: {
+              buildImage: codeBuild.LinuxBuildImage.STANDARD_2_0,
+              privileged: true,
+              environmentVariables: wrappedFlattenedConfig
+            },
+            buildSpec: codeBuild.BuildSpec.fromObject({
+              version: 0.2,
+              phases: {
+                install: {
+                  'runtime-versions': {
+                    docker: 18
+                  }
+                },
+                pre_build: {
+                  commands: [container.loginCommand]
+                },
+                build: {
+                  commands: [container.buildCommand]
+                },
+                post_build: {
+                  commands: [container.pushCommand]
+                }
+              }
+            })
+          }
+        );
+        const buildAction = new codePipelineActions.CodeBuildAction({
+          actionName: 'CodeBuild',
+          input: sourceOutput,
+          project
+        });
+
+        if (container.repo) container.repo.grantPullPush(project);
+        buildActions.push(buildAction);
+      });
+
+      pipeline.addStage({
+        stageName: 'Mu-Build',
+        actions: buildActions
+      });
+    }
   }
 }
 
