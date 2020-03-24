3cb598a5a3a763ce7590e223c56848bff4a74c30
Sepehr Laal <sepehr.laal@gmail.com>
2020-03-24 13:44:56 -0700

    added SQS support under storage

--- lib/resources/storage.ts
+++ lib/resources/storage.ts
@@ -1,4 +1,5 @@
 import * as s3 from '@aws-cdk/aws-s3';
+import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 import assert from 'assert';
 import debug from 'debug';
@@ -6,17 +7,18 @@ import _ from 'lodash';
 
 enum StorageProvider {
   S3 = 's3',
+  SQS = 'sqs',
 }
 
 interface StorageProps {
   provider?: StorageProvider;
-  config?: s3.BucketProps;
+  config?: s3.BucketProps | sqs.QueueProps;
 }
 
 /** Storage provider for service constructs */
 export class Storage extends cdk.Construct {
   public readonly props: StorageProps;
-  public readonly resource: s3.Bucket;
+  public readonly resource: s3.Bucket | sqs.Queue;
   private readonly _debug: debug.Debugger;
 
   /**
@@ -36,10 +38,16 @@ export class Storage extends cdk.Construct {
     this._debug('creating storage with props: %o', this.props);
     switch (this.props.provider) {
       case StorageProvider.S3:
-        this.resource = new s3.Bucket(this, 'bucket', {
+        this.resource = new s3.Bucket(this, 'Bucket', {
           removalPolicy: cdk.RemovalPolicy.DESTROY,
           encryption: s3.BucketEncryption.S3_MANAGED,
-          ...this.props.config,
+          ...(this.props.config as s3.BucketProps),
+        });
+        break;
+      case StorageProvider.SQS:
+        this.resource = new sqs.Queue(this, 'Queue', {
+          encryption: sqs.QueueEncryption.KMS_MANAGED,
+          ...(this.props.config as sqs.QueueProps),
         });
         break;
       default:
