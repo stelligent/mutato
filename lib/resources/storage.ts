172c5d4e21bca4bb67ab0a4b18e19ef6e508b15b
Sepehr Laal <sepehr.laal@gmail.com>
2020-03-24 12:05:26 -0700

    basic s3 / storage construct checked in

---
+++ lib/resources/storage.ts
@@ -0,0 +1,49 @@
+import * as s3 from '@aws-cdk/aws-s3';
+import * as cdk from '@aws-cdk/core';
+import assert from 'assert';
+import debug from 'debug';
+import _ from 'lodash';
+
+enum StorageProvider {
+  S3 = 's3',
+}
+
+interface StorageProps {
+  provider?: StorageProvider;
+  config?: s3.BucketProps;
+}
+
+/** Storage provider for service constructs */
+export class Storage extends cdk.Construct {
+  public readonly props: StorageProps;
+  public readonly bucket?: s3.Bucket;
+  private readonly _debug: debug.Debugger;
+
+  /**
+   * @hideconstructor
+   * @param scope
+   * @param id
+   * @param props
+   */
+  constructor(scope: cdk.Construct, id: string, props: StorageProps) {
+    super(scope, id);
+
+    this._debug = debug(`mu:constructs:storage:${id}`);
+    this.props = _.defaults(props, {
+      provider: StorageProvider.S3,
+    });
+
+    this._debug('creating storage with props: %o', this.props);
+    switch (this.props.provider) {
+      case 's3':
+        this.bucket = new s3.Bucket(this, 'bucket', {
+          removalPolicy: cdk.RemovalPolicy.DESTROY,
+          encryption: s3.BucketEncryption.S3_MANAGED,
+          ...this.props.config,
+        });
+        break;
+      default:
+        assert.fail('storage type not supported');
+    }
+  }
+}
