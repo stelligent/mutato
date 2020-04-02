8ae12b28a1870cf116ea9b85703615e30d590ff1
Sepehr Laal <sepehr.laal@gmail.com>
2020-04-01 22:01:02 -0700

    switching this back, getting too many errors from CDK. will fix npm instead

--- bin/cdk-app.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// utility for command line to print the cdk app
-const fs = require('fs');
-
-if (fs.existsSync('./out/bin/mutato.js'))
-  console.log('node ./out/bin/mutato.js');
-else console.log('npx ts-node ./bin/mutato.ts');
