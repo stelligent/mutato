07fc505eb3c91f9e8a772f3396308436da1023af
Sepehr Laal <sepehr.laal@gmail.com>
2020-04-07 12:30:09 -0700

    added a helper deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#! bash -eux
+
+VAULT_PROFILE=${1-"mutato-dev"}
+docker build --no-cache -t stelligent/mutato .
+echo "NPM_TOKEN=$NPM_TOKEN" > mutato.env
+echo "GITHUB_TOKEN=$GITHUB_TOKEN" > mutato.env
+aws-vault exec $VAULT_PROFILE -- env | grep AWS_ >> mutato.env
+docker run -it --rm --env-file mutato.env -v `pwd`:/project stelligent/mutato deploy
