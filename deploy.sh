37b8360fec8f7bf1698eb263f7e6a4441e87d03d
Sepehr Laal <sepehr.laal@gmail.com>
2020-04-07 13:41:30 -0700

    forgot to add docker username and password to deploy helper

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ VAULT_PROFILE=${1-"mutato-dev"}
 docker build --no-cache -t stelligent/mutato .
 echo "NPM_TOKEN=$NPM_TOKEN" > mutato.env
 echo "GITHUB_TOKEN=$GITHUB_TOKEN" >> mutato.env
+echo "DOCKER_USERNAME=$DOCKER_USERNAME" >> mutato.env
+echo "DOCKER_PASSWORD=$DOCKER_PASSWORD" >> mutato.env
 aws-vault exec $VAULT_PROFILE -- env | grep AWS_ >> mutato.env
 docker run -it --rm --env-file mutato.env -v `pwd`:/project stelligent/mutato deploy
